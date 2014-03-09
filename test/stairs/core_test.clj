3ec5721827b97d8e29e23a7784e7631f910856db
patrick brisbin <pbrisbin@gmail.com>
2014-03-08 21:43:55 -0500

    Use are, always pass k-factor

--- test/stairs/core_test.clj
+++ test/stairs/core_test.clj
@@ -5,16 +5,20 @@
 
 (deftest new-rating-test
   (testing "calculates a player's new rating"
-    (is (= 2115
-           (new-rating {:old-rating 2100, :score 1, :expected-score 0.5, :k-factor 30})))
-    (is (= 2109
-           (new-rating {:old-rating 2100, :score 1, :expected-score 0.69, :k-factor 30})))))
+    (are [expected, old-rating score expected-score]
+         (= expected (new-rating {:old-rating old-rating,
+                                  :score score,
+                                  :expected-score expected-score,
+                                  :k-factor k-factor}))
+         2115 2100 1 0.5
+         2109 2100 1 0.69)))
 
 (deftest expected-score-test
   (testing "calculates the expected score"
-    (are [expected player-rating opponent-rating] 
-         (= expected (expected-score {:player-rating player-rating
-                                      :opponent-rating opponent-rating}))
+    (are [expected player-rating opponent-rating]
+         (= expected (expected-score {:player-rating player-rating,
+                                      :opponent-rating opponent-rating,
+                                      :k-factor k-factor}))
          0.5  1400 1400
          0.53 1420 1400
          0.47 1400 1420
